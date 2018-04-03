eceab70dc1fd2af77f1714cadd63d2db0a4d196f
ao-shen <ao.shen@gmail.com>
2018-04-03 19:49:08 -0400

    FIrst commit

---
+++ Pokeman.py
@@ -0,0 +1,10 @@
+## -------------------------------------- ##
+## This is the main file
+## Run this file on the server
+## -------------------------------------- ##
+## Pokéman Showdown is an original game, unlike PokémOn Showdown who copied our idea.
+
+b_game_on = True
+
+while b_game_on:
+    print("game on")
